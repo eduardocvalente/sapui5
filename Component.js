1b594d35be1159e78b2545e90fecca6be1182f83
eduardocvalente <eduardocvalente1@hotmail.com>
2025-02-27 11:12:18 -0300

    rodando.

--- Component.js
+++ Component.js
@@ -2,10 +2,12 @@ sap.ui.define([
     "sap/ui/core/UIComponent"
 ], function (UIComponent) {
     "use strict";
+
     return UIComponent.extend("myapp.Component", {
         metadata: {
             manifest: "json"
         },
+
         init: function () {
             UIComponent.prototype.init.apply(this, arguments);
         }
