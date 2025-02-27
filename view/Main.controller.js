fc58f30f1422b9896d509c547f3c5646c8a6c003
eduardocvalente <eduardocvalente1@hotmail.com>
2025-02-27 10:20:35 -0300

    novas alterações.

---
+++ view/Main.controller.js
@@ -0,0 +1,12 @@
+
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("myapp.view.Main", {
+        onPress: function () {
+            MessageToast.show("Button Pressed!");
+        }
+    });
+});
